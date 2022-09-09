337af08bbb78ba4e449c6e40bc019b88e5d3b9b3
Diego <diego@MacBook-Pro-de-Diego.local>
2022-09-08 21:29:51 -0300

    teste e2e, configuracao do istanbul, regras de coverage

--- aula04-E2E/api.js
+++ aula04-E2E/api.js
@@ -1,10 +1,25 @@
 const http = require('http')
 
+const DEFAULT_USER = {username: "Bill", password: "123"}
 
 const routes = {
     '/contact:get': (req, res) => {
         res.write('contact us')
         return res.end()
+    },
+
+    '/login:post': async (req, res) => {
+        //res é um iterator !
+        for await (const data of req){
+            const user = JSON.parse(data)
+            if(user.username !== DEFAULT_USER.username || user.password !== DEFAULT_USER.password){
+                res.writeHead(401)
+                res.write("Login failed!")
+                return res.end()
+            }
+            res.write("login has succeeded")
+            return res.end()
+        }
     },
 
     default: (req, res) => {
@@ -17,6 +32,7 @@ const handler = function(req, res){
     const {url, method } = req
     const routeKey = `${url}:${method.toLowerCase()}`
     const chosen = routes[routeKey] || routes.default
+
 
     res.writeHead(200, {
         'Content-Type': 'text/html'
--- aula04-E2E/api.test.js
+++ aula04-E2E/api.test.js
@@ -1,4 +1,6 @@
+//nos ajuda a construir nossas suits de teste separadas
 const {describe, it} = require('mocha')
+//supertest simula as chamadas http
 const request = require('supertest')
 const assert = require('assert')
 
@@ -19,6 +21,23 @@ describe('API test suit', () => {
                     .get('/hello')
                     .expect(200)
             assert.deepStrictEqual(resp.text, 'hello default')
+        })
+    })
+    describe('/login', () => {
+        it('sould login successfully on the login route and return http 200', async() => {
+            const resp = await request(app)
+                    .post('/login')
+                    .send({username: "Bill", password: "123"})
+                    .expect(200)
+            assert.deepStrictEqual(resp.text, 'login has succeeded')
+        })
+        it('sould login unauthorize on the login route and return http 401', async() => {
+            const resp = await request(app)
+                    .post('/login')
+                    .send({username: "Russel", password: "321"})
+                    .expect(401)
+            assert.ok(resp.unauthorized)
+            assert.deepStrictEqual(resp.text, 'Login failed!')
         })
     })
 })
